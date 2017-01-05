a71897bfbfee2ac2855fdfbf364180d8e97a3816
Jason Zhang <jz1242@gmail.com>
2017-01-05 13:06:13 -0500

    practice

--- QuadraticEquationSolverPython/QuadraticSolver.py
+++ QuadraticEquationSolverPython/QuadraticSolver.py
@@ -14,4 +14,4 @@ elif d == 0:
 else:
     x1 = (-b+math.sqrt((b**2)-(4*(a*c))))/(2*a)
     x2 = (-b-math.sqrt((b**2)-(4*(a*c))))/(2*a)
-    print ("This equation has two solutions: ", x1, " or", x2)
+    print ("This equation has two solutions: ", x1, " or", x2)#gg
